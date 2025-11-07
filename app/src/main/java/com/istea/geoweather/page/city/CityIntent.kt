55267bac9ce43753c476d731660b460f97c1d97a
Nicolas Fox <147558766+NicoFox01@users.noreply.github.com>
2025-11-07 08:15:22 -0300

    Update CityIntent.kt
    change de name of getGPS for get DevicePosition and remove the coments

--- app/src/main/java/com/istea/geoweather/page/city/CityIntent.kt
+++ app/src/main/java/com/istea/geoweather/page/city/CityIntent.kt
@@ -1,7 +1,7 @@
 package com.istea.geoweather.page.city
 
 sealed class CityIntent{
-    object  getGPS:CityIntent() //get the lat and long from the GPS
-    data class searchCity(val text:String):CityIntent() //make a call to the API to get the city
-    data class showWeather(val text:String):CityIntent() // make a call to the API to get the weather of the city and go to WeatherPage
+    object  getDevicePosition:CityIntent()
+    data class searchCity(val text:String):CityIntent()
+    data class showWeather(val text:String):CityIntent()
 }
