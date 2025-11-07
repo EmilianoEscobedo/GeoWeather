e4665e05cfb2efb4008b821a95a400bfad8c5827
Nicolas Fox <147558766+NicoFox01@users.noreply.github.com>
2025-11-07 08:27:14 -0300

    Update WeatherState.kt
    modify the comments structure

--- app/src/main/java/com/istea/geoweather/page/weather/WeatherState.kt
+++ app/src/main/java/com/istea/geoweather/page/weather/WeatherState.kt
@@ -1,11 +1,16 @@
 package com.istea.geoweather.page.weather
 
 import com.istea.geoweather.entity.Forecast
-
+// TODO
+// If I mark a city as favorite, the "isFavorite" state should change to true 
+// and the city should be added to the list of favorite cities.
+// If the weather data of a city comes from the favorite cities list, 
+// then "isFavorite" must be true.
+//
+// By default, "forecastButton" should be false. 
+// If the user changes it to true, the forecast weather list should be displayed.
 data class WeatherState (
     val isFavorite: Boolean = false,
-    // si lo hago favorito, tiene que cambiar a true y llevarlo al listado de favoritos de city
-    // Si viene del listado de city, tiene que entrar directamente como true.
     val city: String,
     val temperature: String,
     val humidity: String,
@@ -13,6 +18,5 @@ data class WeatherState (
     val tempMax: Double,
     val dateTime: Long,
     val forecastButton: Boolean = false,
-    //Por default siempre va a venir como false. Puedo vovlerlo true para que me muestre la lista de forecastweather
     val forecastWeatherList: List<Forecast>
 )
