e992ea3755df6279dcbb43b8e0d6e53a66a8ed69
NicoFox01 <nicolas.g1992@psi.uba.ar>
2025-11-11 09:40:38 -0300

    GW-23: Refactor CityState for device location support
    - Add boolean flag to check for geolocation permission status.
    - Add latitude and longitude variables for fetching the current location's weather.

--- app/src/main/java/com/istea/geoweather/page/city/CityState.kt
+++ app/src/main/java/com/istea/geoweather/page/city/CityState.kt
@@ -5,10 +5,10 @@ import com.istea.geoweather.entity.City
 data class CityState(
     val text: String,
     val filterList: List<City>,
-    val favoriteCityList: List<City>
-
-    //wich states from the City entity we will need were?
+    val favoriteCityList: List<City>,
+    val geolocationAllowed: Boolean = true,
     val city: String,
     val country: String,
-    val temperature: String,
+    val latitude: Double,
+    val longitude: Double
     )
