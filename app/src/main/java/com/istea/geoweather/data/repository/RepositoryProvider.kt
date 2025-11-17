b5c5dd3898d75aef935486323b901aa43e9234df
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2025-11-17 17:31:15 -0300

    GW-11: Use singleton provider for repositories

---
+++ app/src/main/java/com/istea/geoweather/data/repository/RepositoryProvider.kt
@@ -0,0 +1,19 @@
+package com.istea.geoweather.data.repository
+
+import com.istea.geoweather.data.api.OpenWeatherClient
+
+object RepositoryProvider {
+    private val openWeatherService = OpenWeatherClient.api
+
+    val cityRepository: CityRepository by lazy {
+        CityRepository(service = openWeatherService)
+    }
+
+    val weatherRepository: WeatherRepository by lazy {
+        WeatherRepository(service = openWeatherService)
+    }
+
+    val forecastRepository: ForecastRepository by lazy {
+        ForecastRepository(service = openWeatherService)
+    }
+}
