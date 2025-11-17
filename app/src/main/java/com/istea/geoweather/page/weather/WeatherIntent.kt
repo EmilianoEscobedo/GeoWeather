2ab8700a51c8482d7aea237531b1f75b86af3b97
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2025-11-17 17:29:27 -0300

    GW-11: Renamed Weather page to Forecast page

--- app/src/main/java/com/istea/geoweather/page/weather/WeatherIntent.kt
+++
@@ -1,9 +0,0 @@
-package com.istea.geoweather.page.weather
-
-sealed class WeatherIntent{
-    object NavigateBack: WeatherIntent()
-    object addCityToFavorite: WeatherIntent() //Add City to the list IF it not in the list and isFavorite = true
-    object removeCityFromFavorite: WeatherIntent() // Remove City from the list IF it not in the list and isFavorite = false
-    object retry: WeatherIntent() //it will fetch the data again from the server
-}
-
