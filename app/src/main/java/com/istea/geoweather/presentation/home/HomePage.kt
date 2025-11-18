f5d9775e3d04caa750345cc7c4d4fc04ccb3b4f3
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2025-11-17 23:14:21 -0300

    GW-13: Move navigation logic from Page to ViewModel

--- app/src/main/java/com/istea/geoweather/presentation/home/HomePage.kt
+++ app/src/main/java/com/istea/geoweather/presentation/home/HomePage.kt
@@ -5,24 +5,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.istea.geoweather.router.Router
-import com.istea.geoweather.router.Route
 
 @Composable
 fun HomePage(
-    router: Router,
-    viewModel: HomeViewModel = viewModel()
+    router: Router
 ) {
+    val factory = HomeViewModelFactory(router)
+    val viewModel: HomeViewModel = viewModel(factory = factory)
     val state by viewModel.state.collectAsState()
 
     HomeView(
         state = state,
-        onIntent = { intent ->
-            when (intent) {
-                HomeIntent.NavigateToCity -> router.navigate(Route.City)
-                HomeIntent.OpenAboutDialog -> viewModel.onIntent(intent)
-                HomeIntent.CloseAboutDialog -> viewModel.onIntent(intent)
-                HomeIntent.FinishLoading -> viewModel.onIntent(intent)
-            }
-        }
+        onIntent = viewModel::onIntent
     )
 }
